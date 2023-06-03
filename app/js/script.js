3903a6657db920ad72b6c2e6278cdc924b074a08
YOUSUFABDI <yufis0844@gmail.com>
2023-06-03 18:55:12 +0300

    added overview charts

--- app/js/script.js
+++ app/js/script.js
@@ -147,6 +147,123 @@ function handlePageLinkAnchors() {
   this.classList.add("dash-active");
 }
 
+// Chart One
+var xValues = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July"];
+var yValues = [55, 49, 44, 24, 30, 45];
+var barColors = [
+  "#35324e",
+  "hsl(210, 79%, 46%)",
+  "hsl(36, 100%, 69%)",
+  "#FF6378",
+  "#115293",
+  "#35324e",
+];
+
+new Chart("myChart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [
+      {
+        backgroundColor: barColors,
+        data: yValues,
+      },
+    ],
+  },
+  options: {
+    legend: { display: false },
+    title: {
+      display: true,
+      text: "Production",
+    },
+  },
+});
+
+//Chart Two
+var xValues = ["Jan", "Feb", "Mar", "Apr"];
+var yValues = [55, 49, 44, 24, 15];
+var barColors = ["#115293", "#4791DB", "#FEBE5F", "#e8c3b9"];
+
+new Chart("myChartTwo", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [
+      {
+        backgroundColor: barColors,
+        data: yValues,
+      },
+    ],
+  },
+  options: {
+    title: {
+      display: true,
+      text: "STATISTICS",
+    },
+  },
+});
+
+//Chart Three
+var xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+var yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+
+new Chart("myChartThree", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [
+      {
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "rgba(0,0,255,1.0)",
+        borderColor: "rgba(0,0,255,0.1)",
+        data: yValues,
+      },
+    ],
+  },
+  options: {
+    legend: { display: false },
+    scales: {
+      yAxes: [{ ticks: { min: 6, max: 16 } }],
+    },
+  },
+});
+
+//Chart Four
+var xyValues = [
+  { x: 50, y: 7 },
+  { x: 60, y: 8 },
+  { x: 70, y: 8 },
+  { x: 80, y: 9 },
+  { x: 90, y: 9 },
+  { x: 100, y: 9 },
+  { x: 110, y: 10 },
+  { x: 120, y: 11 },
+  { x: 130, y: 14 },
+  { x: 140, y: 14 },
+  { x: 150, y: 15 },
+];
+
+new Chart("myChartFour", {
+  type: "scatter",
+  data: {
+    datasets: [
+      {
+        pointRadius: 4,
+        pointBackgroundColor: "rgb(0,0,255)",
+        data: xyValues,
+      },
+    ],
+  },
+  options: {
+    legend: { display: false },
+    scales: {
+      xAxes: [{ ticks: { min: 40, max: 160 } }],
+      yAxes: [{ ticks: { min: 6, max: 16 } }],
+    },
+  },
+});
+
 searchButton.addEventListener("click", openSearchInput);
 closeInputButton.addEventListener("click", closeSearchInput);
 window.addEventListener("resize", handleScreenResize);
